0dc2a4176916bd35bae78f5045d5657e27e49d61
AlexGaiser <alexcgaiser@gmail.com>
2018-12-12 15:39:52 -0500

    ducks "

--- duckhunt.js
+++ duckhunt.js
@@ -1,21 +1,48 @@
-window.onload = function() {
+// window.onload = function() {
   const body = document.body;
-
   console.log(body);
 
   // 1. Create a <div> with the class "duck" and add it to the body.  Do this step by step
   // ( 1. create the element
   //   2. add a class to the element
   //   3. append the element to the body )
+// let divDuck = document.createElement('div')
+
+// divDuck.classList.add("duck")
+  
+// body.appendChild(divDuck)
+
+
 
   // 2. Next, use setInterval to toggle the "flap" class on the duck every 250 ms (1/4 second)
   // https://www.w3schools.com/jsref/met_win_setinterval.asp
+
+function toggleFlap (duck){
+    duck.classList.toggle("flap")
+}
+
 
   // 3. Fantastic!  Now, let's move the duck using CSS "top" and "left". Create
   // a function `moveDuck` that takes a duck object as an argument and sets the
   // "top" and "left" CSS properties.
   // HINT: Use Math.random() * window.innerWidth    for "left"
   //       And Math.random() * window.innerHeight   for "top"
+function moveDuck(duck){
+    duckLeft = Math.random() * window.innerWidth
+    duckTop = Math.random() * window.innerHeight
+
+    duck.style.left = duckLeft.toString() + "px"
+    duck.style.top = duckTop.toString() + "px"
+    if (duckLeft >= window.innerWidth /2){
+      duck.classList.add("left")
+      duck.classList.remove("right")
+    } 
+    else if (duckLeft < window.innerWidth /2){
+      duck.classList.add("right")
+      duck.classList.remove("left")
+    }
+ }
+
 
   // 4. Try making the duck move to a different location every second (what did we use to do this several lines up??)
 
@@ -26,21 +53,76 @@ window.onload = function() {
   // 6. Things are getting a bit messy. Let's create
   //    a "function" called createDuck() that does everything in 1-4
   //    and "returns" the duck object
+function randomLocation(duck){
+  let startNum = (Math.random() * window.innerWidth)  
+  duck.style.left = startNum.toString()+"px"
+  duck.style.top = (window.innerHeight)+ "px"
+}
+
+function createDuck(){
+  newDuck = document.createElement('div')
+  newDuck= newDuck.cloneNode(false)
+  newDuck.classList.add("duck")
+  body.appendChild(newDuck)
+  newDuck.addEventListener('mouseover', addShot)
+  randomLocation(newDuck)
+  // setInterval(toggleFlap, 250, newDuck)
+  setInterval(moveDuck, 250, newDuck)
+  return newDuck
+}
+function addShot(){
+  this.classList.add("shot")
+  let duck = this
+  setTimeout(function(){duck.classList.remove("duck")},1500)
+}
+
 
   // 7. Now, let's create lots of ducks!  Use a "for" loop to create 5 ducks
   //    using our fancy new createDuck() function
+for (i=0; i<5; i++){ createDuck()}
+
+
+// ducks = document.querySelectorAll('.duck')
+// console.log(ducks)
+// for(i=0; i<ducks.length; i++){
+//     console.log(ducks[i])
+
+//     ducks[i].addEventListener('mouseover', addShot)
+// }
+function checkForWinner(){
+  let stillDucks = document.querySelectorAll('.duck')
+  console.log(stillDucks)
+  if (stillDucks.length==0){
+      console.log('winner!')
+  }
+  else{
+    console.log('still ducks')
+  }
+}
+setInterval(checkForWinner, 200)
+
+
+setInterval(moveDuck, 250, newDuck)
+// };
 
   // 8. Uh oh, our ducks are overlapping.  Modify createDuck so each time
   //     it creates a duck, it appears in a random location
   // HINT: You may want to create a `randomPosition()` function that you can use
   //       to set the ducks' initial locations and in your `moveDuck()` function;
 
+
+
+
   // 9. Keep going! Move onto part 3!
+
+
 
   // --------------------------- PART 3 ------------------------------------
 
   // 11. BOOM. Attach a "click" handler that adds the "shot" class to
   //     the duck when you click on it!
+
+
 
   // 12. After a duck has been clicked on, remove it from the DOM after
   //     a short delay (1 second) Hint Hint...use setTimeout
@@ -56,4 +138,3 @@ window.onload = function() {
   //     direction the duck is flying and change the way the duck is facing
 
   // FIN. You win 1 trillion tokens.  Play the day away!
-};
